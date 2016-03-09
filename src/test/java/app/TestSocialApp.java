07d8496126c208b419e403d7e97db4b7c4e19b32
Paddy <pemcgovern@gmail.com>
2016-03-09 11:43:16 +0100

    Clean up.

--- src/test/java/app/TestSocialApp.java
+++ src/test/java/app/TestSocialApp.java
@@ -3,27 +3,11 @@ package app;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TestSocialApp {
 
 	private final SocialApp app = new SocialApp();
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-	@Before
-	public void setUpStreams() {
-		System.setOut(new PrintStream(outContent));
-	}
-
-	@After
-	public void cleanUpStreams() {
-	    System.setOut(null);
-	}
 
 	@Test
 	public void subscriberReceivesPublishersPostsToTimelineWhenFollowing() {
